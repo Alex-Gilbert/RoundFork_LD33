477362905a3809401d7c9e1e5b9c9df1731f5c24
Alex-Gilbert <alexander.paul.gilbert@gmail.com>
2015-08-21 19:20:44 -0500

    test

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	    /*
+         * Just a  quick test of how this works and if it looks good at all....
+         * 
+         * 
+         * I really hope it does :P
+         * */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
